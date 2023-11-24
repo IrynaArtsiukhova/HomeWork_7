58f42ddf7fd0d7991627ee6f69e5586a67eba24e
IrynaArtsiukhova <vasilek.vib@gmail.com>
2023-11-24 16:20:54 +0300

    Added Exercise_1

---
+++ Exsample_1/HomeWork_2/Exercise_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//* Написать программу, которая принимает на вход число и проверяет
+// кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
+
+int x = int.Parse(Console.ReadLine ()!);
+
+if (x % 7 == 0 && x % 23 == 0)
+    Console.Write($"Число {x} кратно одновременно 7 и 23.");
+else
+{
+     Console.Write($"Число {x} НЕ кратно одновременно 7 и 23.");
+}
+Console.WriteLine((int.Parse(Console.ReadLine()!) % 161 == 0));
