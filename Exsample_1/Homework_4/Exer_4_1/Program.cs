15f37dbc43c12c4422480b56587560d089adb525
IrynaArtsiukhova <vasilek.vib@gmail.com>
2023-12-07 12:28:37 +0300

    Added Exer_4

---
+++ Exsample_1/Homework_4/Exer_4_1/Program.cs
@@ -0,0 +1,52 @@
+﻿// // Задача 1: 
+// Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, 
+// сумма цифр которого чётная.
+
+
+while (true)
+{
+    
+    Console.WriteLine("Введите любое число от 10 до 999. Для завершения введите 'q' или число, сумма цифр которой четная:");
+    string input = Console.ReadLine()!;
+
+     if (input.ToLower() == "q")
+    {
+        Console.WriteLine("Вы ввели 'q', программа завершена");
+        break;
+    }
+
+        if (int.TryParse(input, out int number))
+    {
+        
+            int digitSum = CDS(number);
+
+                if (digitSum % 2 == 0)
+        {
+            Console.WriteLine($"Сумма цифр числа {number} чётная. Программа завершена.");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Продолжайте ввод.");
+        }
+    }
+    else
+    {
+        
+        Console.WriteLine("Некорректный ввод.");
+    }
+}
+
+
+// Метод для вычисления суммы цифр числа.
+int CDS(int num) // CDS = CalculateDigitSum
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
