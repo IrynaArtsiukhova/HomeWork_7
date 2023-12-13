7f0f9ab32e173e2ca4308549fd2010351677dbb7
IrynaArtsiukhova <vasilek.vib@gmail.com>
2023-12-13 21:42:35 +0300

    Added Ex_5_1

--- Exsample_1/Homework_5/Exer_5_1/Program.cs
+++ Exsample_1/Homework_5/Exer_5_1/Program.cs
@@ -2,98 +2,31 @@
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 
-static void Main()
-    {
-        Console.Write("Введите количество строк массива: ");
-        int numRows;
-         while (!int.TryParse(Console.ReadLine(), out numRows) || numRows <= 0)
-        {
-            Console.WriteLine("Ошибка. Введите положительное число строк массива.");
-         }
-
-        Console.Write("Введите количество столбцов массива: ");
-        int numCols;
-
-        while (!int.TryParse(Console.ReadLine(), out numCols) || numCols <= 0)
-        {
-            Console.WriteLine("Ошибка. Введите положительное число столбцов массива.");
-        }
-
-        // Генерируем двумерный массив, заполненный случайными числами
-        int[,] randomNumbersArray = GenerateRandomNumbersArray(numRows, numCols);
-
-        Console.WriteLine("Сгенерированный массив:");
-        DisplayArray(randomNumbersArray);  
-
-        Console.Write("\nВведите номер строки: ");
-        int row;
-
-        while (!int.TryParse(Console.ReadLine(), out row) || row < 0 || row >= numRows)
-        {
-            Console.WriteLine($"Ошибка. Введите номер строки (0 to {numRows - 1}).");
-        }
-
-        Console.Write("Введите номер столбца: ");
-        int col;
-
-        while (!int.TryParse(Console.ReadLine(), out col) || col < 0 || col >= numCols)
-        {
-            Console.WriteLine($"Ошибка. Введите номер столбца (0 to {numCols - 1}).");
-        }
-
-        int elementValue = GetElementValue(randomNumbersArray, row, col);
-        if (elementValue != int.MinValue)
-        {
-            Console.WriteLine($"\nЗначение элемента ({row}, {col}): {elementValue}");
-        }
-        else
-        {
-            Console.WriteLine($"\nТакого элемента нет ({row}, {col}).");
-        }
-    }
-
-    static int[,] GenerateRandomNumbersArray(int numRows, int numCols)
-    {
-        Random random = new Random();
-        int[,] numbersArray = new int[numRows, numCols];
-
-         for (int i = 0; i < numRows; i++)
-        {
-            for (int j = 0; j < numCols; j++)
-            {
-                numbersArray[i, j] = random.Next(1, 100); // Генерируем случайное число в диапазоне от 1 до 100
-            }
-        }
-
-        return numbersArray;
-    }
-
-    static void DisplayArray(int[,] array)
-    {
-        int numRows = array.GetLength(0);
-        int numCols = array.GetLength(1);
-
-        for (int i = 0; i < numRows; i++)
-        {
-            for (int j = 0; j < numCols; j++)
-            {
-                Console.Write(array[i, j] + " ");
-            }
-            Console.WriteLine();
-        }
-    }
-
-    static int GetElementValue(int[,] array, int row, int col)
-    {
-        int numRows = array.GetLength(0);
-        int numCols = array.GetLength(1);
-
-        if (row >= 0 && row < numRows && col >= 0 && col < numCols)
-        {
-            return array[row, col];
-        }
-        else
-        {
-            return int.MinValue; // Указывает на отсутствие такого элемента
-        }
-    }
+Console.Clear();
+Console.Write("Введите строку: ");
+int row = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int col = Convert.ToInt32(Console.ReadLine()) - 1;
+int x = 10; 
+int y = 10; 
+Random random = new Random();
+int[,] arr = new int[x, y];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 100);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (row < 0 | row > arr.GetLength(0) - 1 | col < 0 | col > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Такого элемента нет ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[row, col]);
+}
+Console.ReadLine();
